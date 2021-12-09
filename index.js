6220e2f39e7dde1c123d773196a7f0e8f69b5ea1
Timur Kim <allrighty1809@gmail.com>
2021-12-09 11:31:14 +0600

    update

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
-const express = require('express');
-const  kmlParse = require('kml-parse');
+// const express = require('express');
+const kmlParse = require('kml-parse');
 const fs = require('fs-extra');
 const DOMParser = require('xmldom').DOMParser;
 
-const app = express();
+// const app = express();
 
-app.get('/', (req, res) => {
-	res.json(getJson());
-})
+// app.get('/', (req, res) => {
+// 	res.json(getJson());
+// })
 
-app.listen(3000, () => {
-	console.log('Server running at port 3000');
-})
+// app.listen(3000, () => {
+// 	console.log('Server running at port 3000');
+// })
 
-function getJson() {
-  const kmlDom = new DOMParser().parseFromString(
-    fs.readFileSync("assets/cosule.kml", "utf-8")
-  );
+// function getJson() {
+//   const kmlDom = new DOMParser().parseFromString(
+//     fs.readFileSync("assets/cosule.kml", "utf-8")
+//   );
 
-  let result = kmlParse.parseGeoJSON(kmlDom);
+//   let result = kmlParse.parseGeoJSON(kmlDom);
 
-  const foo = result.features.filter(x => x.geometry.coordinates != undefined);
+//   const foo = result.features.filter(x => x.geometry.coordinates != undefined);
 
-  foo.map(x => {
-    if (x.geometry.type == 'Polygon') {
-      x.geometry.coordinates[0] = x.geometry.coordinates[0].map(cords => {
-        return [cords[1], cords[0], cords[2]];
-      });
-    }
-  })
+//   foo.map(x => {
+//     if (x.geometry.type == 'Polygon') {
+//       x.geometry.coordinates[0] = x.geometry.coordinates[0].map(cords => {
+//         return [cords[1], cords[0], cords[2]];
+//       });
+//     }
+//   })
 
-  result.features = foo;
+//   result.features = foo;
   
-  return result; 
-}
+//   return result; 
+// }
 
 writeJson();
 
@@ -46,13 +46,13 @@ function writeJson() {
 
   const foo = result.features.filter(x => x.geometry.coordinates != undefined);
 
-  foo.map(x => {
-    if (x.geometry.type == 'Polygon') {
-      x.geometry.coordinates[0] = x.geometry.coordinates[0].map(cords => {
-        return [cords[1], cords[0], cords[2]];
-      });
-    }
-  })
+  // foo.map(x => {
+  //   if (x.geometry.type == 'Polygon') {
+  //     x.geometry.coordinates[0] = x.geometry.coordinates[0].map(cords => {
+  //       return [cords[1], cords[0], cords[2]];
+  //     });
+  //   }
+  // })
 
   result.features = foo;
 
